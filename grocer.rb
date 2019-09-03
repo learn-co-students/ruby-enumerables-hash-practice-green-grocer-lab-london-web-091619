ea061e5396a9f2dc573e07decb710c4b12e6b079
Kane Pangalia Stephens <kanepstephens@gmail.com>
2019-09-03 10:50:10 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,20 @@
 def consolidate_cart(cart)
-  # code here
+  con_cart = Hash.new
+  
+  cart.each { |item|
+    product_name = item.keys[0]
+    product_value = item.values[0]
+  
+    if con_cart.has_key?(product_name) then
+      con_cart[product_name][:count] += 1
+    else  
+      con_cart[product_name] = product_value
+      con_cart[product_name][:count] = 1
+    end
+  }
+  
+  print(con_cart)
+  return con_cart
 end
 
 def apply_coupons(cart, coupons)
