373b0fb73355f87283f698f75c0930df497021d2
bogomilmihaylov <bogomilmihaylov@live.com>
2019-09-10 19:25:39 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,36 @@
+require 'pry'
+
 def consolidate_cart(cart)
-  # code here
+final_hash = { }
+cart.each do |element_hash|
+ element_name = element_hash.keys[0]
+
+ if final_hash.has_key?(element_name)
+   final_hash[element_name][:count] += 1
+  else
+    final_hash[element_name] = {
+      count: 1,
+      price: element_hash[element_name][:price],
+      clearance: element_hash[element_name][:clearance]
+    }
+  end
+ end
+final_hash
 end
 
 def apply_coupons(cart, coupons)
-  # code here
-end
+  coupons.each do |coupon|
+  item = coupon[:item]
+  if cart[item] 
+    if cart[item][:count] >= coupon[:num] && !cart.has_key?["#{item} W/COUPON"]
+      cart["#{item} W/COUPON"] = {price: coupon[:cost] / coupon[:num],clearance: cart[item][:clearance], count: coupon[:num] }
+      cart[item][:count] -= coupon[:num]
+elsif cart[item][:count] >= coupon[:num] && cart.has_key?["#{item} W/COUPON"]
+      cart["#{item} W/COUPON"][:count] += coupon[:num]
+      cart[item][:count] -= coupon[:num]
 
-def apply_clearance(cart)
-  # code here
+    end
+  end
 end
-
-def checkout(cart, coupons)
-  # code here
-end
+cart
+end  
